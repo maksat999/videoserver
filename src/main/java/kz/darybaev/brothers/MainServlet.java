42b3afadd5c756808299f3deb6076137e82b2b30
Maksat Darybayev <maksatd.k@gmail.com>
2016-02-06 10:47:26 +0600

    init

---
+++ src/main/java/kz/darybaev/brothers/MainServlet.java
@@ -0,0 +1,112 @@
+package kz.darybaev.brothers;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
+
+
+
+public class MainServlet extends HttpServlet{
+    private Random random = new Random();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("name"," by Darybaev Company");
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  /*     String description = req.getParameter("description"); // Retrieves <input type="file" name="videofile">
+       String data = req.getParameter("data");*/
+
+//проверяем является ли полученный запрос multipart/form-data
+        boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+        if (!isMultipart) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        // Создаём класс фабрику
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        // Максимальный буфера данных в байтах,
+        // при его привышении данные начнут записываться на диск во временную директорию
+        // устанавливаем один мегабайт
+        factory.setSizeThreshold(1024*1024);
+
+        // устанавливаем временную директорию
+        File tempDir = (File)getServletContext().getAttribute("javax.servlet.context.tempdir");
+        factory.setRepository(tempDir);
+
+        //Создаём сам загрузчик
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        //максимальный размер данных который разрешено загружать в байтах
+        //по умолчанию -1, без ограничений. Устанавливаем 10 мегабайт.
+        upload.setSizeMax(1024 * 1024 * 10);
+
+        try {
+            List items = upload.parseRequest(req);
+            Iterator iter = items.iterator();
+
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+
+                if (item.isFormField()) {
+                    //если принимаемая часть данных является полем формы
+                    processFormField(item);
+                } else {
+                    //в противном случае рассматриваем как файл
+                    processUploadedFile(item);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+    }
+
+    /**
+     * Сохраняет файл на сервере, в папке upload.
+     * Сама папка должна быть уже создана.
+     *
+     * @param item
+     * @throws Exception
+     */
+    private void processUploadedFile(FileItem item) throws Exception {
+        File uploadetFile = null;
+        //выбираем файлу имя пока не найдём свободное
+        do{
+            String path = getServletContext().getRealPath("/upload/"+random.nextInt() + item.getName());
+            uploadetFile = new File(path);
+        }while(uploadetFile.exists());
+
+        //создаём файл
+        uploadetFile.createNewFile();
+        //записываем в него данные
+        item.write(uploadetFile);
+    }
+
+    /**
+     * Выводит на консоль имя параметра и значение
+     * @param item
+     */
+    private void processFormField(FileItem item) {
+        System.out.println(item.getFieldName()+"="+item.getString());
+    }
+
+
+
+    }
---
+++ src/main/webapp/mypage.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Максат
+  Date: 03.02.2016
+  Time: 19:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Brothers.kz</title>
+</head>
+<body>
+
+<h1>Video Hosting Server ${name}</h1>
+
+<form action="/" enctype="multipart/form-data" method="post">
+    <p><input type="file" name="videofile">
+        <input type="description" type="text">
+        <input type="submit" value="Отправить"></p>
+</form>
+</body>
+</html>
